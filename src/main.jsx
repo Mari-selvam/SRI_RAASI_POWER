5f100de1447a0133c12f77c8f0956ab1fb69054e
Mari-selvam <mariselvam.blue@gmail.com>
2024-01-02 16:55:49 +0530

    add image

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Nav from './nav.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+container = document.getElementById("nav")
+root = ReactDOM.createRoot(container);
+root.render(
+<React.StrictMode><Nav/></React.StrictMode>,)
+
---
+++ src/nav.jsx
@@ -0,0 +1,15 @@
+function Nav(){
+    return(
+        <>
+        <div>
+
+            <h1>NAV</h1>
+        </div>
+        
+        
+        </>
+    )
+
+}
+
+export default Nav;
